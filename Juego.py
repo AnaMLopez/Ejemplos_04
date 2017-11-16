73981ef92c2724bae8125e0def92cd62a35df5ee
Roberto Martínez <rmroman@itesm.mx>
2017-11-16 09:03:04 -0600

    Juego con Sprites

--- Juego.py
+++ Juego.py
@@ -1,6 +1,7 @@
 # encoding: UTF-8
 # Autor: Roberto Martínez Román
 # Muestra cómo utilizar pygame para escribir programas que dibujan en la pantalla
+from random import randint
 
 import pygame
 
@@ -13,57 +14,58 @@ VERDE_BANDERA = (0, 122, 0)
 ROJO = (255, 0, 0)
 
 
-def dibujarMenu(ventana, btnJugar):
-    ventana.blit(btnJugar.image, btnJugar.rect)
+def dibujarMenu(ventana, botonJugar):
+    ventana.blit(botonJugar.image, botonJugar.rect)
 
 
-def dibujarJuego(ventana, btnJugar, lista, listaBalas):
-    btnJugar.rect.left = ANCHO-btnJugar.rect.width
-    btnJugar.rect.top = ALTO-btnJugar.rect.height
-    ventana.blit(btnJugar.image, btnJugar.rect)
-    # Dibujamos TODOS los enemigos
-    for enemigo in lista:
+def dibujarJuego(ventana, listaEnemigos, listaBalas):
+    for enemigo in listaEnemigos:
         ventana.blit(enemigo.image, enemigo.rect)
+
     for bala in listaBalas:
         ventana.blit(bala.image, bala.rect)
 
 
 def actualizarBalas(listaBalas, listaEnemigos):
-    for bala in listaBalas:
+    for bala in listaBalas: # NO DEBEN modificar
         bala.rect.top -= 20
-    for e in listaEnemigos:
-        e.rect.top += 1
-    # Eliminar balas fuera de la pantalla
-    for k in range(-1,-len(listaBalas)-1,-1 ):
-        if listaBalas[k].rect.top <= -16:
-            listaBalas.remove(listaBalas[k])
-    # Verificar colisiones
-    for bala in listaBalas:
+        if bala.rect.top <= 0:
+            listaBalas.remove(bala)
+            continue    # REGRESA al inicio del ciclo
         borrarBala = False
-        for enemigo in listaEnemigos:
-
+        for k in range(len(listaEnemigos)-1,-1,-1):
+            enemigo = listaEnemigos[k]
             if bala.rect.colliderect(enemigo):
-                # Le pegó!!!!!!!!!!
-                # INCORRECTO !!!!!!!!!!!
                 listaEnemigos.remove(enemigo)
                 borrarBala = True
-                break   # detener el CICLO for
+                break   # TERMINA el ciclo
         if borrarBala:
             listaBalas.remove(bala)
 
 
-def generarEnemigos(listaEnemigos, imgBotonJugar):
-    for x in range(3):
+def generarEnemigos(listaEnemigos, imgEnemigo):
+    for x in range(5):
         for y in range(4):
-            cx = 10 + x*260
+            # Generar el enemigo en x,y
+            cx = 50 + x*150
             cy = 50 + y*100
-            nuevo = pygame.sprite.Sprite()
-            nuevo.image = imgBotonJugar
-            nuevo.rect = imgBotonJugar.get_rect()
-            nuevo.rect.left = cx
-            nuevo.rect.top = cy
-            listaEnemigos.append(nuevo)
+            enemigo = pygame.sprite.Sprite()
+            enemigo.image = imgEnemigo
+            enemigo.rect = imgEnemigo.get_rect()
+            enemigo.rect.left = cx
+            enemigo.rect.top = cy
+            listaEnemigos.append(enemigo)
 
+
+def generarEnemigoAzar(listaEnemigos, imgEnemigo):
+    cx = randint(20,ANCHO-128)
+    cy = randint(50,ALTO)
+    enemigo = pygame.sprite.Sprite()
+    enemigo.image = imgEnemigo
+    enemigo.rect = imgEnemigo.get_rect()
+    enemigo.rect.left = cx
+    enemigo.rect.top = cy
+    listaEnemigos.append(enemigo)
 
 
 def dibujar():
@@ -73,70 +75,85 @@ def dibujar():
     reloj = pygame.time.Clock() # Para limitar los fps
     termina = False # Bandera para saber si termina la ejecución
 
-    # Estados
-    estado = "menu"     # jugando, termina
+    estado = "menu"    # jugando, fin
 
-    # Botones
-    imgBotonJugar = pygame.image.load("botonJugar.png")
-    btnJugar = pygame.sprite.Sprite()   # SPRITE
-    btnJugar.image = imgBotonJugar
-    btnJugar.rect = imgBotonJugar.get_rect()
-    btnJugar.rect.left = ANCHO//2 - btnJugar.rect.width//2
-    btnJugar.rect.top = ALTO//2 - btnJugar.rect.height//2
+    # Cargar imágenes
+    imgBtnJugar = pygame.image.load("botonJugar.png")
+    # Sprite
+    botonJugar = pygame.sprite.Sprite()
+    botonJugar.image = imgBtnJugar
+    botonJugar.rect = imgBtnJugar.get_rect()
+    botonJugar.rect.left = ANCHO//2 - botonJugar.rect.width//2
+    botonJugar.rect.top = ALTO//2 - botonJugar.rect.height//2
 
     # Enemigos
     listaEnemigos = []
-    generarEnemigos(listaEnemigos, imgBotonJugar)
+    imgEnemigo = pygame.image.load("enemigo.png")
+    generarEnemigos(listaEnemigos, imgEnemigo)
 
     # Balas
     listaBalas = []
     imgBala = pygame.image.load("bala.jpg")
 
+    # Tiempos
     timer = 0
+
+    # MUSICA de fondo
+    pygame.mixer.init()
+    pygame.mixer.music.load("musicaFondo.mp3")
+    pygame.mixer.music.play(-1)
+
+    efecto = pygame.mixer.Sound("shoot.wav")
 
     while not termina:
         # Procesa los eventos que recibe
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:  # El usuario hizo click en el botón de salir
                 termina = True
-            elif evento.type == pygame.MOUSEBUTTONDOWN:
+            elif evento.type == pygame.MOUSEBUTTONDOWN: # El usuario hizo click
                 xm, ym = pygame.mouse.get_pos()
                 if estado == "menu":
-                    xb, yb, anchoB, altoB = btnJugar.rect
+                    xb, yb, anchoB, altoB = botonJugar.rect
                     if xm>=xb and xm<=xb+anchoB:
                         if ym>=yb and ym<=yb+altoB:
+                            # Cambiar de ventana
                             estado = "jugando"
                 elif estado == "jugando":
-                    nuevo = pygame.sprite.Sprite()
-                    nuevo.image = imgBotonJugar
-                    nuevo.rect = imgBotonJugar.get_rect()
-                    nuevo.rect.left = xm
-                    nuevo.rect.top = ym
-                    listaEnemigos.append(nuevo)
+                    enemigo = pygame.sprite.Sprite()
+                    enemigo.image = imgEnemigo
+                    enemigo.rect = imgEnemigo.get_rect()
+                    enemigo.rect.left = xm
+                    enemigo.rect.top = ym
+                    listaEnemigos.append(enemigo)
             elif evento.type == pygame.KEYDOWN:
                 if evento.key == pygame.K_SPACE:
-                    # crear bala
+                    efecto.play()
                     bala = pygame.sprite.Sprite()
                     bala.image = imgBala
                     bala.rect = imgBala.get_rect()
                     bala.rect.left = ANCHO//2
-                    bala.rect.top = ALTO-bala.rect.height
+                    bala.rect.top = ALTO - bala.rect.height
                     listaBalas.append(bala)
 
         # Borrar pantalla
         ventana.fill(BLANCO)
 
+        fuente = pygame.font.SysFont("monospace", 48)
+        texto = fuente.render("Hola! :) " + str(int(timer*100)), 1, VERDE_BANDERA)
+        ventana.blit(texto, (ANCHO // 2 - 100, ALTO // 2))
+
+        # Generar enemigos cada 2 segundos
         timer += 1/40
-        if timer > 2:
-            # Acciones
+        if timer>=2:
             timer = 0
+            generarEnemigoAzar(listaEnemigos, imgEnemigo)
 
         # Dibujar, aquí haces todos los trazos que requieras
         if estado == "menu":
-            dibujarMenu(ventana, btnJugar)
+            dibujarMenu(ventana, botonJugar)
         elif estado == "jugando":
             actualizarBalas(listaBalas, listaEnemigos)
-            dibujarJuego(ventana, btnJugar, listaEnemigos, listaBalas)
+            dibujarJuego(ventana, listaEnemigos, listaBalas)
 
         pygame.display.flip()   # Actualiza trazos
         reloj.tick(40)          # 40 fps
